be74141db558a3ff9f363abcfd115d06ea0df0ec
Richard Fox <rjfox321@gmail.com>
2016-04-04 12:25:33 +0100

    Initial placeholder IEnemy and LevelSection scripts

---
+++ Assets/Scripts/LevelSection.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelSection : MonoBehaviour
+{
+    public IList<IEnemy> Enemies;
+    public int EnemiesLeft;
+
+	// Use this for initialization
+	void Start()
+	{
+	    EnemiesLeft = Enemies.Count;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	    if(EnemiesLeft <=0)
+            TriggerBlend();
+	}
+
+    void TriggerBlend()
+    {
+        
+    }
+}
