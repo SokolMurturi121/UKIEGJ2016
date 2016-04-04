dc96cd31756548da56119de8478e07a1f00789e1
Richard Fox <rjfox321@gmail.com>
2016-04-04 13:58:31 +0100

    Add main scene and iso camera

--- Assets/Scripts/LevelSection.cs
+++ Assets/Scripts/LevelSection.cs
@@ -4,24 +4,35 @@ using System.Collections.Generic;
 
 public class LevelSection : MonoBehaviour
 {
-    public IList<IEnemy> Enemies;
+    public List<Enemy> Enemies;
     public int EnemiesLeft;
+
+    private float blendTime;
 
 	// Use this for initialization
 	void Start()
 	{
 	    EnemiesLeft = Enemies.Count;
+	    blendTime = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update()
     {
-	    if(EnemiesLeft <=0)
-            TriggerBlend();
+	    if (EnemiesLeft <= 0)
+	    {
+
+	        
+	    }
+
+
 	}
 
     void TriggerBlend()
     {
-        
+        foreach (Enemy enemy in Enemies)
+        {
+            
+        }
     }
 }
